594b431b55eb17edc63b9963fe105de9df78fe57
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-06-20 15:54:47 +0200

    PlatformConfig: Fix UART console parameters
    The serial console params were wrong: fix them, so that we don't
    drive developers crazy when they need it... :)))

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -39,7 +39,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 
 # Serial console
-#BOARD_KERNEL_CMDLINE += earlycon=msm_serial_dm,0xc170000
+#BOARD_KERNEL_CMDLINE += earlycon=msm_geni_serial,0x4a90000 console=ttyMSM0,115200n8
 
 TARGET_RECOVERY_WIPE := $(PLATFORM_COMMON_PATH)/rootdir/recovery.wipe
 TARGET_RECOVERY_FSTAB ?= $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/fstab.seine
