sonyxperiadev/device-sony-seine

commit d47d786f3eb8255a89789a81628ab1a421d00e01

platform: Move several vars into platform.mk
Defining these variables earlier means we can make use of
them in `$(inherit)`-ed lowercase makefiles.

Also, move the include of common.mk to the end of the file,
so that common.mk can make use of the variables defined
above.

Signed-off-by: Haxk20 <16738302+Haxk20@users.noreply.github.com>

platform.mk
@@ -19,7 +19,70 @@ TRINKET := sm6125
 SOMC_PLATFORM := seine
 SOMC_KERNEL_VERSION := 4.14
 
+PRODUCT_PLATFORM_SOD := true
+
+TARGET_BOARD_PLATFORM := $(TRINKET)
+
 SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
+
+TARGET_PD_SERVICE_ENABLED := true
+
+# Wi-Fi definitions for Qualcomm solution
+WIFI_DRIVER_BUILT := qca_cld3
+WIFI_DRIVER_DEFAULT := qca_cld3
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+HOSTAPD_VERSION := VER_0_8_X
+WIFI_DRIVER_FW_PATH_AP  := "ap"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+TARGET_USES_ICNSS_QMI := true
+WIFI_DRIVER_STATE_CTRL_PARAM := "/sys/kernel/boot_wlan/boot_wlan"
+WIFI_DRIVER_STATE_OFF := 0
+WIFI_DRIVER_STATE_ON := 1
+
+# BT definitions for Qualcomm solution
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+TARGET_USE_QTI_BT_STACK := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_COMMON_PATH)/bluetooth
+WCNSS_FILTER_USES_SIBS := true
+
+# RIL
+TARGET_PER_MGR_ENABLED := true
+
+# NFC
+NXP_CHIP_FW_TYPE := PN557
+
+# Audio
+BOARD_SUPPORTS_SOUND_TRIGGER := true
+AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
+AUDIO_FEATURE_ENABLED_CUSTOMSTEREO := true
+AUDIO_FEATURE_ENABLED_ACDB_LICENSE := false
+AUDIO_FEATURE_ENABLED_BATTERY_LISTENER := true
+AUDIO_FEATURE_ENABLED_USB_BURST_MODE := true
+AUDIO_FEATURE_SONY_CIRRUS := true
+
+# DSP
+TARGET_NEEDS_AUDIOPD := true
+TARGET_NEEDS_SENSORS_PDR := true
+
+# SSC Sensors
+TARGET_USES_SSC := true
+
+# Display
+TARGET_HAS_HDR_DISPLAY := true
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+TARGET_USES_DRM_PP := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
+
+# Lights HAL: Backlight
+TARGET_USES_SDE := true
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \

PlatformConfig.mk
@@ -12,12 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Platform path
-PLATFORM_COMMON_PATH := device/sony/seine
-PRODUCT_PLATFORM_SOD := true
-
-TARGET_BOARD_PLATFORM := $(TRINKET)
-
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -45,63 +39,8 @@ BOARD_KERNEL_CMDLINE += msm_drm.blhack_dsi_display0=dsi_panel_somc_seine_cmd:con
 TARGET_RECOVERY_WIPE := $(PLATFORM_COMMON_PATH)/rootdir/recovery.wipe
 TARGET_RECOVERY_FSTAB ?= $(PLATFORM_COMMON_PATH)/rootdir/vendor/etc/fstab.seine
 
-TARGET_PD_SERVICE_ENABLED := true
-
-# Wi-Fi definitions for Qualcomm solution
-WIFI_DRIVER_BUILT := qca_cld3
-WIFI_DRIVER_DEFAULT := qca_cld3
-BOARD_HAS_QCOM_WLAN := true
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
-BOARD_WLAN_DEVICE := qcwcn
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
-HOSTAPD_VERSION := VER_0_8_X
-WIFI_DRIVER_FW_PATH_AP  := "ap"
-WIFI_DRIVER_FW_PATH_P2P := "p2p"
-WIFI_DRIVER_FW_PATH_STA := "sta"
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-TARGET_USES_ICNSS_QMI := true
-WIFI_DRIVER_STATE_CTRL_PARAM := "/sys/kernel/boot_wlan/boot_wlan"
-WIFI_DRIVER_STATE_OFF := 0
-WIFI_DRIVER_STATE_ON := 1
-
-# BT definitions for Qualcomm solution
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_QCOM := true
-TARGET_USE_QTI_BT_STACK := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_COMMON_PATH)/bluetooth
-WCNSS_FILTER_USES_SIBS := true
-
-# RIL
-TARGET_PER_MGR_ENABLED := true
-
-# NFC
-NXP_CHIP_FW_TYPE := PN557
-
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_COMMON_PATH)/sepolicy_platform
-
-# Audio
-BOARD_SUPPORTS_SOUND_TRIGGER := true
-AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
-AUDIO_FEATURE_ENABLED_CUSTOMSTEREO := true
-AUDIO_FEATURE_ENABLED_ACDB_LICENSE := false
-AUDIO_FEATURE_ENABLED_BATTERY_LISTENER := true
-AUDIO_FEATURE_ENABLED_USB_BURST_MODE := true
-AUDIO_FEATURE_SONY_CIRRUS := true
-
-# SSC Sensors
-TARGET_USES_SSC := true
-
-# Display
-TARGET_HAS_HDR_DISPLAY := true
-TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
-TARGET_USES_DRM_PP := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
-
-# Lights HAL: Backlight
-TARGET_USES_SDE := true
 
 # Build a separate vendor.img
 TARGET_COPY_OUT_VENDOR := vendor

commit 2f1a3afcad7f01de311a47c9d369738ea3160398

platform.mk: Remove duplicate and wrong DSP
This found its way in one of my commits and should be removed.

Fixes: d47d786f3eb8255a89789a81628ab1a421d00e01

platform.mk
@@ -68,10 +68,6 @@ AUDIO_FEATURE_ENABLED_BATTERY_LISTENER := true
 AUDIO_FEATURE_ENABLED_USB_BURST_MODE := true
 AUDIO_FEATURE_SONY_CIRRUS := true
 
-# DSP
-TARGET_NEEDS_AUDIOPD := true
-TARGET_NEEDS_SENSORS_PDR := true
-
 # SSC Sensors
 TARGET_USES_SSC := true
 