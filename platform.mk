6e5360010abd63cfb152191b8c765f33068d7996
Alin Jerpelea <alin.jerpelea@sony.com>
2021-09-07 11:03:18 +0200

    platform: add hvdcp service
    the hvdcp service is moved to platform
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- platform.mk
+++ platform.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
     init.qcom.cdspstart.sh \
     cdsprpcd.rc
 
+# HVDCP init
+PRODUCT_PACKAGES += \
+    hvdcp_opti.rc
+
 # Audio init
 PRODUCT_PACKAGES += \
     audiopd.rc
