007b7f6049cd816196db013e06af0af4ceaa9bca
ShujathMohd <shujath@outlook.in>
2023-10-30 14:35:12 +0530

    platform: Swith to AOSP android.hardware.bluetooth@1.0-service HAL We are currentl using android.hardware.bluetooth@1.0-service-qti which is from Android 9 Switch to the latest HAL.

--- platform.mk
+++ platform.mk
@@ -249,7 +249,7 @@ PRODUCT_PACKAGES += \
 # QCOM Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service-qti
+    android.hardware.bluetooth@1.0-service
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qcom.bluetooth.soc=cherokee
