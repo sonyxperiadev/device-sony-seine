867be4abb02695169265739a82e206fba1ff2456
Pavel Dubrova <pashadubrova@gmail.com>
2025-03-03 07:59:01 +0200

    platform: Remove RQBalance config
    RQBalance requires deep integration with the kernel
    and has not been supported for several years. Remove
    its configuration file that are not even tuned for
    the current platform.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- platform.mk
+++ platform.mk
@@ -186,10 +186,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/vendor/etc/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
-# RQBalance-PowerHAL configuration
-PRODUCT_COPY_FILES += \
-    $(SONY_ROOT)/vendor/etc/rqbalance_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/rqbalance_config.xml
-
 # Platform specific init
 PRODUCT_PACKAGES += \
     tad.rc \
